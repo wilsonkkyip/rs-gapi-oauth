0aada04abc21bf4ed9b7e2306f3e5994c5bfd72c
Wilson <wilson@Wilsons-MBP.local>
2023-07-26 20:41:47 +0000

    commit

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -29,43 +29,6 @@ async fn test_auth_user() {
         .expect("Unable to authenticate");
 }
 
-#[test]
-fn test_variables() {
-    let client_email = std::env::var("SERVICE_CLIENT_EMAIL")
-        .expect("No SERVICE_CLIENT_EMAIL in env var")
-        .as_str().to_string();
-    let private_key = std::env::var("SERVICE_PRIVATE_KEY")
-        .expect("No SERVICE_PRIVATE_KEY in env var")
-        .as_str().to_string();
-    let private_key_id = std::env::var("SERVICE_PRIVATE_KEY_ID")
-        .expect("No SERVICE_PRIVATE_KEY_ID in env var")
-        .as_str().to_string();
-    let client_id = std::env::var("USER_CLIENT_ID")
-        .expect("No USER_CLIENT_ID in env var")
-        .as_str().to_string();
-    let client_secret = std::env::var("USER_CLIENT_SECRET")
-        .expect("No USER_CLIENT_SECRET in env var")
-        .as_str().to_string();
-    let refresh_token = std::env::var("USER_REFRESH_TOKEN")
-        .expect("No USER_REFRESH_TOKEN in env var")
-        .as_str().to_string();
-    let xxx = std::env::var("CARGO_TEAM_COLOR")
-        .expect("No CARGO_TEAM_COLOR in env var")
-        .as_str().to_string();
-    let TEST_SECRET = std::env::var("TEST_SECRET")
-        .expect("No TEST_SECRET in env var")
-        .as_str().to_string();
-
-    assert_eq!(TEST_SECRET, "hi");
-    assert!(client_email.len() > 10);
-    assert!(private_key.len() > 10);
-    assert!(private_key_id.len() > 10);
-    assert!(client_id.len() > 10);
-    assert!(client_secret.len() > 10);
-    assert!(refresh_token.len() > 10);
-
-}
-
 #[tokio::test]
 async fn test_auth_service() {
     let client_email = std::env::var("SERVICE_CLIENT_EMAIL")
